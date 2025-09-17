ee2d97d9f27d7884c26506620c8238bc9c0d9c0f
Akash Singh <121565348+akash9569@users.noreply.github.com>
2025-09-18 03:34:45 +0530

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,13 +5,12 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyDgKmiST7Lq8oZ9oqViHMRLrADgbjsFDvY",
+  authDomain: "studio-6099401193-3e7d1.firebaseapp.com",
+  projectId: "studio-6099401193-3e7d1",
+  storageBucket: "studio-6099401193-3e7d1.firebasestorage.app",
+  messagingSenderId: "816107923052",
+  appId: "1:816107923052:web:bf641a93a4d28e0abb5a49"
 };
 
 // Initialize Firebase
